c9d26342c7818519f7e9e76766b300bd45634eed
Maxim <u04@asv.me>
2024-02-27 17:39:26 +0300

    feature(map-demo): add Ruller and RullerPolygon
    add classes Ruller, RullerPolygon and auxiliary class DisposableReactiveObject
    
    asana: https://app.asana.com/0/1203851531040615/1206551986581995/f

---
+++ src/Asv.Avalonia.Map.Demo/ViewModels/RullerPolygon.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Asv.Common;
+using Avalonia.Collections;
+using Avalonia.Media;
+using DynamicData;
+
+namespace Asv.Avalonia.Map.Demo;
+
+public class RulerPolygon : MapAnchorViewModel
+{
+    private readonly ReadOnlyObservableCollection<GeoPoint> _path;
+    
+    public RulerPolygon(Ruler ruler)
+    {
+        Ruler = new RxValue<Ruler>();
+        
+        ZOrder = -1000;
+        OffsetX = 0;
+        OffsetY = 0;
+        PathOpacity = 0.6;
+        StrokeThickness = 5;
+        Stroke = Brushes.Purple;
+        IsVisible = false;
+        StrokeDashArray = new AvaloniaList<double>(2,2);
+
+        var cache = new SourceList<GeoPoint>();
+        cache.Add(new GeoPoint(0, 0, 0));
+        cache.Add(new GeoPoint(0, 0, 0));
+
+        ruler.IsVisible.Where(_ => _.HasValue).Subscribe(_ => IsVisible = _.Value);
+        ruler.Start.Where(_ => _.HasValue).Subscribe(_ => cache.ReplaceAt(0, _.Value));
+        ruler.Stop.Where(_ => _.HasValue).Subscribe(_ => cache.ReplaceAt(1, _.Value));
+
+        cache.Connect()
+            .Bind(out _path)
+            .Subscribe();
+        
+        Ruler.OnNext(ruler);
+    }
+
+    public override ReadOnlyObservableCollection<GeoPoint> Path => _path;
+    
+    public IRxEditableValue<Ruler> Ruler { get; }
+}
